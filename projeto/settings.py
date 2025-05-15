baa1b7fa03175be383758fb8dd8c1d114673926e
GustavoLenni <133364786+GustavoLenni@users.noreply.github.com>
2025-05-15 17:54:23 -0300

    commit postgres

--- projeto/settings.py
+++ projeto/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +79,10 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://banco_de_dados_pisv_user:HjkbIZMj7vamg5lhKrw1aALOh5YwLe5v@dpg-d0j501qli9vc73banlcg-a.oregon-postgres.render.com/banco_de_dados_pisv'
+    )
+    
 }
 
 
